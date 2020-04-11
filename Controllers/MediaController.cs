92f06dd462ff98dcaf345cb6b44fc6aa21c86774
jarmatys <jarekarmatys@gmail.com>
2020-04-11 15:47:23 +0200

    create media controller + add and list view

---
+++ Controllers/MediaController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Controllers
+{
+    public class MediaController : Controller
+    {
+        // [ GET ] - <domain>/Media/List
+        [HttpGet]
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        // [ GET ] - <domain>/Media/Add
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
