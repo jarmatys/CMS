e38ca26d1eb05fffde2795208b99085ffe3deddb
jarmatys <jarekarmatys@gmail.com>
2020-04-11 17:38:36 +0200

    Add comment controller and list view

---
+++ Controllers/CommentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Controllers
+{
+    public class CommentController : Controller
+    {
+
+        // [ GET ] - <domain>/Comment/List
+        [HttpGet]
+        public IActionResult List()
+        {
+            return View();
+        }
+
+    }
+}
