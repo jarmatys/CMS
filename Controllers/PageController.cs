78f1b68d5bec2d6f1a0fe6710984d96d4d530b4b
jarmatys <jarekarmatys@gmail.com>
2020-04-11 17:15:55 +0200

    Add Page Controller + Add and List View

---
+++ Controllers/PageController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Controllers
+{
+    public class PageController : Controller
+    {
+        // [ GET ] - <domain>/Article/List
+        [HttpGet]
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        // [ GET ] - <domain>/Article/Add
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
