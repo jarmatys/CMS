31bc08dd3bbd29262d15002502f05e7343ee27a3
jarmatys <jarekarmatys@gmail.com>
2020-04-11 14:37:25 +0200

    Create Category controller + list and add view

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // [ GET ] - <domain>/Category/List
+        [HttpGet]
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        // [ POST ] - <domain>/Category/Add
+        [HttpPost]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
