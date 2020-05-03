14769f5945a37f03c73bf54741a83c1961940bdc
jarmatys <jarekarmatys@gmail.com>
2020-05-03 16:27:44 +0200

    Add Payment controller

---
+++ CMS/Controllers/PaymentController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Controllers
+{
+    [Authorize]
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
